bafb7dbfa5cc33a5041e8d9a53ec24ac56a1db64
coffer <zbq19930108@126.com>
2020-06-22 10:00:18 +0800

    code message : 优化启动

--- app/src/main/java/com/coffer/qq/activity/BaseApp.java
+++ app/src/main/java/com/coffer/qq/activity/BaseApp.java
@@ -1,6 +1,10 @@
 package com.coffer.qq.activity;
 
+import android.app.ActivityManager;
 import android.app.Application;
+import android.content.Context;
+
+import com.FrameWork;
 
 /**
  * @author：张宝全
@@ -12,4 +16,50 @@ import android.app.Application;
  */
 
 public class BaseApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        /**
+         * Application的优化
+         * 1.必要的组件在程序主页去初始化
+         * 2.如果组件一定要在App中初始化，那么尽可能的延时
+         * 3.非必要的组件，子线程中初始化
+         */
+
+        //只在主进程中初始化
+        if (getApplicationInfo().packageName.equals(
+                getCurProcessName(getApplicationContext()))) {
+            //获取渠道
+            //String flavor = FlavorHelper.getFlavor(this);
+            //Toast.makeText(this, "flavor:" + flavor, Toast.LENGTH_SHORT).show();
+            FrameWork.getInstance().initFramework(this);
+        }
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+//        MultiDex.install(this);
+    }
+
+    /**
+     * 获取当前进程名
+     *
+     * @param context
+     * @return
+     */
+    public static String getCurProcessName(Context context) {
+        int pid = android.os.Process.myPid();
+        ActivityManager activityManager = (ActivityManager)
+                context.getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningAppProcessInfo appProcess :
+                activityManager.getRunningAppProcesses()) {
+            if (appProcess.pid == pid) {
+                return appProcess.processName;
+            }
+        }
+        return null;
+    }
 }
--- framework/src/main/java/com/FrameWork.java
+++ framework/src/main/java/com/FrameWork.java
@@ -1,5 +1,9 @@
 package com;
 
+import android.content.Context;
+
+import com.utils.SpUtils;
+
 /**
  * @author：张宝全
  * @date：2020-02-02
@@ -25,5 +29,31 @@ public class FrameWork {
             }
         }
         return mFrameWork;
+    }
+
+    /**
+     * 初始化框架 Model
+     *
+     * @param mContext
+     */
+    public void initFramework(Context mContext) {
+        SpUtils.getInstance().initSp(mContext);
+//        BmobManager.getInstance().initBmob(mContext);
+//        CloudManager.getInstance().initCloud(mContext);
+//        LitePal.initialize(mContext);
+//        MapManager.getInstance().initMap(mContext);
+//        WindowHelper.getInstance().initWindow(mContext);
+//        CrashReport.initCrashReport(mContext, BUGLY_KEY, BuildConfig.LOG_DEBUG);
+//        ZXingLibrary.initDisplayOpinion(mContext);
+//        NotificationHelper.getInstance().createChannel(mContext);
+//        KeyWordManager.getInstance().initManager(mContext);
+//
+//        //全局捕获RxJava异常
+//        RxJavaPlugins.setErrorHandler(new Consumer<Throwable>() {
+//            @Override
+//            public void accept(Throwable throwable) throws Exception {
+//                LogUtils.e("RxJava：" + throwable.toString());
+//            }
+//        });
     }
 }
